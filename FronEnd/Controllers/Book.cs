47cc2a1731acaf2947f5c833179fa805145252f2
Andric Vargas <andric@outlook.com>
2017-05-28 12:02:28 -0500

    Ejercicios iniciales en GitHub

---
+++ FronEnd/Controllers/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FronEnd.Controllers
+{
+    public class Book
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string nombre { get; set; }
+    }
+}
---
+++ backend/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Año { get; set; }
+    }
+}
